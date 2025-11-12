05e3664a94cf7fea3984e45b9bbb75607cf22624
Jian Lee <ramosjianlee@gmail.com>
2025-11-12 17:07:46 +0800

    feat: added end points for put and delete methods for users

--- backend/routes/ADUserRoutes.js
+++ backend/routes/ADUserRoutes.js
@@ -6,5 +6,7 @@ const verifyToken = require("../middleware/verifyToken");
 router.post("/register", ADUserController.createUser);
 router.post("/login", ADUserController.logInUser);
 router.get("/profile", verifyToken, ADUserController.getUserById);
+router.put("/profile/:user_id", ADUserController.updateUser);
+router.delete("/profile/:user_id", ADUserController.deleteUser);
 
 module.exports = router;
