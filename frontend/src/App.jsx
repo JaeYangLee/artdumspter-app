c69b0b72693d5e23d205521049beba8ca9ab8df8
Jian Lee <ramosjianlee@gmail.com>
2025-11-13 19:06:36 +0800

    feat: implemented API call endpoints for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import axios from "axios";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdNavBar from "./components/AdNavBar";
@@ -6,6 +8,50 @@ import AdRegisterPage from "./pages/AdRegisterPage";
 import AdLoginPage from "./pages/AdLoginPage";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const registerUser = async (
+    username,
+    email,
+    password,
+    bio,
+    location,
+    tool_id,
+    artstyle_id
+  ) => {
+    try {
+      const res = axios.post(`http://localhost:5000/artDumpster/register`, {
+        username,
+        email,
+        password,
+        bio,
+        location,
+        tool_id,
+        artstyle_id,
+      });
+
+      setUser(res.data);
+      console.log("[POST /App.jsx]: User registration successful!");
+    } catch (err) {
+      console.error("[POST /App.jsx]: Error creating new user!");
+    }
+  };
+
+  const loginUser = async (email, password) => {
+    try {
+      const res = await axios.post(`http://localhost:5000/artDumpster/login`, {
+        email,
+        password,
+      });
+
+      setUser(res.data);
+
+      console.log("[POST /App.jsx]: Log in successful!");
+    } catch (err) {
+      console.error("[POST /App.jsx]: Error logging in user!");
+    }
+  };
+
   return (
     <>
       <AdNavBar />
