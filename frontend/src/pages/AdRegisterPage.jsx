8d5f5f3c5ade1ddb8aefed2ebd7ef72a4541f102
Jian Lee <ramosjianlee@gmail.com>
2025-11-13 18:57:41 +0800

    feat: finished laying out the design template for register page

--- frontend/src/pages/AdRegisterPage.jsx
+++ frontend/src/pages/AdRegisterPage.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function AdRegisterPage() {
   return (
     <>
-      <div className="flex flex-col items-center w-screen h-screen pt-16">
-        <div className="flex flex-col items-center w-full h-full bg-gray-300">
-          <header>Create Art Account</header>
-          <form></form>
-        </div>
+      <div className="flex flex-col items-start justify-center w-screen h-screen px-4 pt-14">
+        <form className="flex flex-col items-start justify-center gap-4 p-4 px-4 border rounded-lg">
+          <h1 className="text-lg font-bold text-left">
+            CREATE NEW ART ACCOUNT
+          </h1>
+          <section>
+            <div>
+              <label>Enter username:</label>
+              <input required type="text" className="px-2 border rounded" />
+            </div>
+            <div>
+              <label>Enter bio:</label>
+              <textarea type="email" className="px-2 border rounded" />
+            </div>
+            <div>
+              <label>Enter location:</label>
+              <input required type="text" className="px-2 border rounded" />
+            </div>
+            <div>
+              <label>Enter email:</label>
+              <input required type="email" className="px-2 border rounded" />
+            </div>
+            <div>
+              <label>Enter password:</label>
+              <input required type="password" className="px-2 border rounded" />
+            </div>
+          </section>
+
+          <section className="flex flex-row items-end justify-end w-full gap-2">
+            <button type="submit" className="px-2 border rounded">
+              Submit
+            </button>
+            <Link to="/" type="button" className="px-2 border rounded">
+              Cancel
+            </Link>
+          </section>
+        </form>
       </div>
     </>
   );
