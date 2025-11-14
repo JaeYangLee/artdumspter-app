8497b63e4f3e21a0c101fa72e644d8a5c3514117
Jian Lee <ramosjianlee@gmail.com>
2025-11-15 07:31:04 +0800

    fix: reset the style templates of the landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdNavBar from "./components/AdNavBar";
+
 import AdLandingPage from "./pages/AdLandingPage";
-import AdRegisterPage from "./pages/AdRegisterPage";
-import AdLoginPage from "./pages/AdLoginPage";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -68,12 +66,9 @@ function App() {
 
   return (
     <>
-      <AdNavBar />
       <Router>
         <Routes>
           <Route exact path="/" element={<AdLandingPage />}></Route>
-          <Route path="/register" element={<AdRegisterPage />}></Route>
-          <Route path="/login" element={<AdLoginPage />}></Route>
         </Routes>
       </Router>
     </>
--- frontend/src/pages/AdLandingPage.jsx
+++ frontend/src/pages/AdLandingPage.jsx
@@ -1,48 +1,14 @@
 import { useState } from "react";
-import { BsArrowRight } from "react-icons/bs";
-import AdAuthModal from "../components/AdAuthModal";
 function AdLandingPage() {
-  const [isAuthModalOpen, setAuthModelOpen] = useState(false);
   return (
     <>
-      <div className="flex flex-col items-center w-screen h-screen md:flex-row pt-14">
-        <div className="flex flex-col items-center justify-center h-full gap-8 p-4 bg-gray-400 md:items-start md:w-full">
-          <section className="flex flex-col items-center justify-center md:items-start">
-            <h1 className="text-4xl font-bold text-center md:text-left">
-              Welcome to ArtDumpster*
-            </h1>
-            <p className="text-sm text-center md:text-left">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-          </section>
-          <section className="flex flex-row items-center justify-center w-full gap-2 md:items-start md:justify-start">
-            <button
-              onClick={() => setAuthModelOpen(true)}
-              className="flex flex-row items-center gap-2 p-2 px-4 text-white bg-black border rounded"
-            >
-              Get Started
-              <span className="hidden md:flex">
-                <BsArrowRight />
-              </span>
-            </button>
-          </section>
+      <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-300">
+        <div className="bg-blue-400">
+          <section></section>
+          <section></section>
         </div>
-
-        <div className="h-full md:w-[40vw] flex flex-col items-center justify-center">
-          <h1 className="text-5xl font-bold">AD*</h1>
-        </div>
+        <div className="w-full bg-red-400"></div>
       </div>
-
-      <AdAuthModal
-        isAuthModalOpen={isAuthModalOpen}
-        onAuthModalClose={() => setAuthModelOpen(false)}
-      />
     </>
   );
 }
