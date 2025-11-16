fb2b42cefde63c65dc94f268140f513bb2f528a5
Jian Lee <ramosjianlee@gmail.com>
2025-11-16 12:04:53 +0800

    style: finished the design template of the landing page

--- frontend/src/pages/AdLandingPage.jsx
+++ frontend/src/pages/AdLandingPage.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
-function AdLandingPage() {
+import { Link } from "react-router-dom";
+
+function AdLandingPage({ onLogin, onRegister }) {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-screen h-screen ">
+      <div className="flex flex-col items-center justify-center w-screen h-screen bg-primary text-secondary">
         <div className="w-full h-full flex flex-col md:flex-row items-center justify-center">
           <section className="w-full h-full flex flex-col items-center justify-center">
             <div className="text-center md:text-start p-4">
@@ -14,26 +15,31 @@ function AdLandingPage() {
           </section>
 
           <section className="w-full h-full flex flex-col items-center justify-center ">
-            <div className="flex flex-col items-center justify-center gap-4 border rounded-lg p-4">
+            <div className="flex flex-col items-center justify-center gap-4 border rounded-lg px-4 py-8 shadow-[4px_4px_0px_0px]">
               <form className="flex flex-col items-center justify-center gap-2 ">
                 <input
                   type="email"
                   placeholder="Enter email..."
-                  className="px-2 border rounded"
+                  className="px-2 border rounded shadow-[2px_2px_0px_0px]"
                 />
                 <input
                   type="password"
                   placeholder="Enter password..."
-                  className="px-2 border rounded"
+                  className="px-2 border rounded shadow-[2px_2px_0px_0px]"
                 />
-                <button className="w-full px-2 border rounded">Log in</button>
+                <button className="w-full px-2 border rounded bg-secondary text-primary shadow-secondary border-secondary shadow-[2px_2px_0px_0px]">
+                  Log in
+                </button>
               </form>
 
-              <hr className="w-full" />
+              <hr className="w-full opacity-50" />
 
-              <button className="px-2 rounded border">
+              <Link
+                to="/register"
+                className="px-2 rounded border bg-accent shadow-[2px_2px_0px_0px]"
+              >
                 Create new art account
-              </button>
+              </Link>
             </div>
           </section>
         </div>
