45da40843f3f77069c491e626d234d95272ca345
Jian Lee <ramosjianlee@gmail.com>
2025-11-18 15:13:05 +0800

    feat: finished building the endpoint routes for artstyles table

--- backend/routes/AdArtStylesRoutes.js
+++ backend/routes/AdArtStylesRoutes.js
@@ -1,1 +1,8 @@
-const AdArtStylesControlle = require("../controller/AdArtStylesController");
+const AdArtStylesController = require("../controller/AdArtStylesController");
+const express = require("express");
+const router = express.Router();
+
+router.get("/artStyles", AdArtStylesController.fetchAllArtStyles);
+router.get("/artStyles/:artstyle_id", AdArtStylesController.fetchArtStyleById);
+
+module.exports = router;
