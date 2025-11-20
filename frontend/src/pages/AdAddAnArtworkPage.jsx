0c31e04e92a7f08516c33ccbc9f7c74d395f384b
Jian Lee <ramosjianlee@gmail.com>
2025-11-20 14:08:35 +0800

    feat: create AdAddAnArtworkPage component with layout and AdNavBar integration

---
+++ frontend/src/pages/AdAddAnArtworkPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AdNavBar from "../components/AdNavBar";
+
+function AdAddAnArtworkPage({}) {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-screen h-screen pt-14">
+        <h1>Add an artwork page</h1>
+      </div>
+      <AdNavBar />
+    </>
+  );
+}
+
+export default AdAddAnArtworkPage;
