aebf7d0e1425563c35c8224e2b98f2db3f4dfeed
Jian Lee <ramosjianlee@gmail.com>
2025-11-20 14:08:29 +0800

    feat: create AdMyDumpsterPage component with layout and AdNavBar integration

---
+++ frontend/src/pages/AdMyDumpsterPage.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import AdNavBar from "../components/AdNavBar";
+
+function AdMyDumpsterPage({}) {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-screen full-screen pt-14">
+        <h1>My Dumpster Page</h1>
+      </div>
+
+      <AdNavBar />
+    </>
+  );
+}
+
+export default AdMyDumpsterPage;
