c9334fd431f50ab387536c16a1c1e6ab3b57f0f5
Jian Lee <ramosjianlee@gmail.com>
2025-11-20 12:52:40 +0800

    feat: enhance AdProfilePage layout and add user information display

--- frontend/src/pages/AdProfilePage.jsx
+++ frontend/src/pages/AdProfilePage.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import AdNavBar from "../components/AdNavBar";
+import { BsPersonCircle } from "react-icons/bs";
+import { BsEnvelope } from "react-icons/bs";
+import { BsFillPinMapFill } from "react-icons/bs";
 
 function AdProfilePage({ onLogout }) {
   const navigate = useNavigate();
@@ -39,13 +42,55 @@ function AdProfilePage({ onLogout }) {
   if (!user) return <p>Loading...</p>;
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-screen h-screen">
-        <h1>{user.username}</h1>
-        <h1>{user.email}</h1>
-        <h1>{user.bio}</h1>
-        <h1>{user.location}</h1>
-        <h1>{user.tool_name}</h1>
-        <h1>{user.artstyle_name}</h1>
+      <div className="flex flex-col md:flex-row justify-center pt-16 w-screen h-screen p-2">
+        <div className="flex flex-col items-center w-full justify-center  rounded py-4 px-2 gap-4 bg-gray-200">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <BsPersonCircle className="text-textColor size-28 opacity-80" />
+            <h1 className="text-lg font-bold">{user.username}</h1>
+            <p className="w-full italic">
+              <span className="opacity-40">" </span>
+              {user.bio}
+              <span> "</span>
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div className="flex flex-row items-center gap-4 w-full justify-start">
+              <BsEnvelope className="size-6 " />
+              <h3>{user.email}</h3>
+            </div>
+            <div className="flex flex-row  w-full justify-start items-center gap-4">
+              <BsFillPinMapFill className="size-6" />
+              <h3>{user.location}</h3>
+            </div>
+
+            <div className="pt-8 w-full">
+              <button className="px-2 rounded border w-full bg-primary text-backgroundColor border-textColor shadow-textColor shadow-[2px_2px_0px_0px]">
+                Edit profile
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full  md:h-full rounded py-4 px-2 gap-4">
+          <div className="flex flex-col w-full items-start justify-start">
+            <h1 className="font-bold">Most used tool:</h1>
+            <h3 className="w-full bg-gray-200 p-2">{user.tool_name}</h3>
+          </div>
+          <div className="flex flex-col w-full items-start justify-start">
+            <h1 className="font-bold">Art style:</h1>
+            <h3 className="w-full bg-gray-200 p-2">{user.artstyle_name}</h3>
+          </div>
+
+          <div className="flex md:h-full items-end">
+            <button
+              onClick={onLogout}
+              className=" w-full border rounded  bg-secondary shadow-[2px_2px_0px_0px]"
+            >
+              Log out
+            </button>
+          </div>
+        </div>
       </div>
 
       <AdNavBar />
