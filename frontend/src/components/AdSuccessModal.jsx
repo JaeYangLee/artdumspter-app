0f4b60363966eb59c1943ac8802e20243c513235
Jian Lee <ramosjianlee@gmail.com>
2025-11-27 21:38:12 +0800

    feat: add AdSuccessModal component for displaying success messages

---
+++ frontend/src/components/AdSuccessModal.jsx
@@ -0,0 +1,43 @@
+import { BsCheckCircleFill } from "react-icons/bs";
+
+function AdSuccessModal({
+  title,
+  subject,
+  message,
+  isSuccessModalOpen,
+  onSuccessModalClose,
+}) {
+  if (!isSuccessModalOpen) return null;
+  return (
+    <>
+      <div
+        onClick={onSuccessModalClose}
+        className="fixed top-0 flex items-center justify-center w-screen h-screen px-4 z-60 bg-black/50"
+      >
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="flex flex-col items-center justify-center w-full p-4 rounded bg-backgroundColor shadow-[4px_4px_0px_0px]"
+        >
+          <section className="flex flex-col items-center justify-center">
+            <BsCheckCircleFill className="text-green-500 size-16 " />
+
+            <h1 className="pt-4 text-lg font-bold">{title}</h1>
+            <p className="text-sm">
+              <span>{subject}</span>
+              {message}
+            </p>
+          </section>
+
+          <button
+            onClick={onSuccessModalClose}
+            className="px-2 mt-8 border rounded bg-primary text-backgroundColor border-black shadow-black shadow-[2px_2px_0px_0px]"
+          >
+            Okay
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AdSuccessModal;
