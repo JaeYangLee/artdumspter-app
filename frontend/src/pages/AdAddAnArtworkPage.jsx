60f24a4502b3ae357d140137c82467542eb0f7e3
Jian Lee <ramosjianlee@gmail.com>
2025-11-27 21:38:21 +0800

    style: update heading color in AdAddAnArtworkPage component

--- frontend/src/pages/AdAddAnArtworkPage.jsx
+++ frontend/src/pages/AdAddAnArtworkPage.jsx
@@ -7,7 +7,9 @@ function AdAddAnArtworkPage({ onLogout }) {
     <>
       <div className="flex flex-col items-center justify-center w-screen h-screen pt-14 text-textColor">
         <div className="flex flex-col items-center w-full h-full p-2 bg-gray">
-          <h1 className="w-full p-2 text-2xl font-bold">Add an artwork</h1>
+          <h1 className="w-full p-2 text-2xl font-bold text-primary">
+            Add an artwork
+          </h1>
 
           <form className="flex flex-col items-center w-full h-full gap-2 p-2">
             <section className="items-start w-full px-2 ">
