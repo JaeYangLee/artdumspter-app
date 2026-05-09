4dabaa9c9b28903f82669eb34b2b499748a02623
Jian Lee <ramosjianlee@gmail.com>
2026-05-10 00:17:06 +0800

    feat: implement AdArtworkController with CRUD operations for artwork management

---
+++ backend/controller/AdArtworkController.js
@@ -0,0 +1,40 @@
+const adArtworkModel = require("../model/AdArtworksModel");
+
+const fetchAllArtwork = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+const fetchArtworkById = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+const fetchArtworkByUser = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+const addArtwork = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+const updateArtwork = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+const deleteArtwork = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+module.exports = {
+  fetchAllArtwork,
+  fetchArtworkById,
+  fetchArtworkByUser,
+  addArtwork,
+  updateArtwork,
+  deleteArtwork,
+};
