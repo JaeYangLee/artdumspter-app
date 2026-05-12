85bdf55520608ff9f3c3bd71af8a0b6f1d86f36a
Jian Lee <ramosjianlee@gmail.com>
2026-05-12 22:53:01 +0800

    feat: implement AdArtworksRoutes for artwork management with CRUD operations

---
+++ backend/routes/AdArtworksRoutes.js
@@ -0,0 +1,35 @@
+const AdArtworkController = require("../controller/AdArtworkController");
+const express = require("express");
+const router = express.Router();
+const uploadConfig = require("../middleware/uploadConfig");
+const verifyToken = require("../middleware/verifyToken");
+
+router.get("/artWorks", verifyToken, AdArtworkController.fetchAllArtwork);
+router.get(
+  "/artWorks/:artwork_id",
+  verifyToken,
+  AdArtworkController.fetchArtworkById,
+);
+router.get(
+  "/artWorks/:user_id",
+  verifyToken,
+  AdArtworkController.fetchArtworkByUser,
+);
+router.post(
+  "/artWork/upload",
+  verifyToken,
+  uploadConfig.single("image_url"),
+  AdArtworkController.addArtwork,
+);
+router.put(
+  "/artWork/edit/:artwork_id",
+  verifyToken,
+  AdArtworkController.updateArtwork,
+);
+router.delete(
+  "/artWork/:artwork_id",
+  verifyToken,
+  AdArtworkController.deleteArtwork,
+);
+
+module.exports = router;
