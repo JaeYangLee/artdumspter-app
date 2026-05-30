b2b40cda1b9459e50273a17ce0fe9bd5d6545ba6
Jian Lee <ramosjianlee@gmail.com>
2026-05-30 19:35:59 +0800

    fix: simplify artwork display logic by utilizing AdArtworkList component in My Dumpster page

--- frontend/src/pages/AdMyDumpsterPage.jsx
+++ frontend/src/pages/AdMyDumpsterPage.jsx
@@ -2,26 +2,15 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AdNavBar from "../components/AdNavBar";
 import { useFetchUserById } from "../hooks/useFetchUserById";
+import AdArtworkList from "../components/AdArtworkList";
 
-function AdMyDumpsterPage({ user, artworks = [], onLogout }) {
+function AdMyDumpsterPage({ user, artworks, onLogout }) {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-screen full-screen pt-14">
+      <div className="flex flex-col items-center w-screen h-screen pt-14">
         <h1>My Dumpster Page</h1>
 
-        {artworks?.length > 0 ? (
-          artworks.map((artwork) => (
-            <div key={artwork.artwork_id}>
-              <img
-                className="w-40 h-40 object-cover"
-                src={`http://localhost:5000/${artwork.image_url}`}
-                alt={artwork.title}
-              />
-            </div>
-          ))
-        ) : (
-          <p className="opacity-60">No artworks yet</p>
-        )}
+        <AdArtworkList artworks={artworks} />
       </div>
 
       <AdNavBar user={user} onLogout={onLogout} />
