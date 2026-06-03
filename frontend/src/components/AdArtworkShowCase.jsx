63517ac0d31a05b4e10a2124ec8b1984be666204
Jian Lee <ramosjianlee@gmail.com>
2026-06-03 21:55:00 +0800

    feat: enhance layout of AdArtworkShowCase for improved user experience

--- frontend/src/components/AdArtworkShowCase.jsx
+++ frontend/src/components/AdArtworkShowCase.jsx
@@ -10,19 +10,19 @@ function AdArtworkShowCase({
     <>
       <div
         onClick={onArtworkShowcaseClose}
-        className="fixed inset-0 top-0 flex flex-col items-center justify-center w-screen h-screen px-4 bg-black/80 z-70"
+        className="fixed inset-0 top-0 flex flex-col items-center justify-start w-screen h-screen px-4 pt-12 md:items-center bg-black/80 z-70"
       >
-        <div className="">
-          <p className="w-full text-backgroundColor">x</p>
+        <div className="flex flex-col items-center justify-center">
+          <p className="w-full text-right text-backgroundColor">x</p>
           <div className="flex flex-col w-full md:flex-row">
-            <section className="w-1/2">
+            <section className="md:w-[70vw] bg-black">
               <img
                 src={`http://localhost:5000/${artwork.image_url}`}
                 alt={artwork.title}
                 className="object-contain"
               />
             </section>
-            <section className="w-1/2 bg-backgroundColor">
+            <section className="md:w-1/2 bg-backgroundColor">
               <p>{artwork.title}</p>
             </section>
           </div>
