85660eee771ad774892650347d3856907c4f9790
Jian Lee Ramos <ramosjianlee@gmail.com>
2026-07-20 15:29:26 +0200

    feat: add new artwork images and log server version on startup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 7000;
 
+console.log("🔥 SERVER VERSION UPDATED");
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
